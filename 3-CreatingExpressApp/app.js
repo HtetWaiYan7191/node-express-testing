5b9e4e722488b5d02a781e7d2355d7d108ba84e0
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2024-01-18 22:42:32 +0630

    Add create user api endpoint

--- 3-CreatingExpressApp/app.js
+++ 3-CreatingExpressApp/app.js
@@ -1,22 +1,52 @@
 const express = require('express')
 const morgan = require('morgan'); // thirdpart middleware instead of app.use()
 const mongoose = require('mongoose'); // third part library for object document mapping (ODM)
+const bodyParser = require('body-parser');
 const Blog = require('./models/blog');
+const User = require('./models/user');
 
 // mongo db connect url
 const dbURL = "mongodb+srv://htetwaiyan7191:719171@node-express-tuto.2vir9km.mongodb.net/?retryWrites=true&w=majority";
-// const dbURL = "mongodb+srv://htetwaiyan7191:719171@node-express-tuto.2vir9km.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((result) => console.log('connected to db'))
+.then((result) => app.listen(3000))
 .catch((err) => console.log(err)); // connect database 
 
 //express app 
 const app = express();
 
+// middleware to parse json data in request body when testing with postman 
+app.use(express.json());
+
 // middleware using morgan library ! 
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 
 // mongoose and mongo sandbox routes 
+
+//list users 
+app.get('/users', (req, res) => {
+    User.find().then((result) => {
+        res.send(result)
+    })
+})
+
+//create user 
+app.post('/users', (req,res) => {
+    console.log('create running...')
+    console.log(req.body);
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+    });
+
+    user.save().then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
 
 app.get('/add-blog', (req, res) => {
     const blog = new Blog({
